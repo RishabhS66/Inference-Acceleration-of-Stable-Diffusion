1138c5c73a932223dc2234eda90b6d3b1a0ded08
Rishabh Srivastava <rishabhsri66@gmail.com>
2024-09-09 22:42:15 -0400

    Update main.py
    Remove confidential api key

--- code/main.py
+++ code/main.py
@@ -13,9 +13,6 @@ from pruning_profile_exp import *
 if __name__ == '__main__':
     ''' Wandb initialization and running experiments'''
 
-    os.environ["WANDB_API_KEY"] = "918907cbd509a54b20e48c35485f867aab3e59df"
-    os.environ["WANDB_HOST"] = "rishabhs"
-
     wandb.login()
     wandb.init(
         project="quantization_pruning",
